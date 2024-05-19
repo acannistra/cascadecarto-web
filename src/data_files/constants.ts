fef190a7df90b2e4734b2c3ce594a83f671c66a6
Tony Cannistra <tony.cannistra@gmail.com>
2024-05-19 11:07:32 -0700

    fixup issues with OG

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,11 +1,11 @@
-import ogImageSrc from "@images/social.png";
+import ogImageSrc from "@images/cc_draft.webp";
 
 export const SITE = {
   title: "Cascade Cartographics",
   tagline: "Fast and Accurate GIS and Mapping Design Services",
-  description: "Cascade Cartographics offers GIS and map design services for projects big and small, using modern techniques for high-quality and efficient delivery.", 
+  description: "Cascade Cartographics offers GIS and map design services for projects big and small, using modern techniques for high-quality and efficient delivery.",
   description_short: "Cascade Cartographics offers top-quality GIS and map design services for small and medium sized projects.",
-  url: "https://cascadecarto.org",
+  url: "http://cascadecarto.org",
   author: "Tony Cannistra",
 };
 
@@ -33,7 +33,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : GIS and Map Design Services`,
+  title: `${SITE.title} : : GIS and Map Design Services`,
   description: "Cascade Cartographics provides accurate and affordable GIS and map design services for small and medium sized projects.",
   image: ogImageSrc,
 };
